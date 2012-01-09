ce74b68f35566ecebc4c672c4ee8abd249c31a96
unknown <George@.faktab.net>
2012-01-09 12:22:43 +0100

    Fixed problem with ancestors

--- fotballfilter.user.js
+++ fotballfilter.user.js
@@ -9,4 +9,4 @@
 // @match        http://www.nettavisen.no/*
 // ==/UserScript==
 
-for(var a=document.evaluate("//a[contains(@href, 'fotball') or contains(@title, 'fotball')]/ancestor::div[contains(@class,'ddCell') or contains(@class,'article-extract') or contains(@class,'gridUnit') or contains(@class,'article_small') or contains(@class,'art_medium')]",document,null,XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,null),b=a.snapshotLength-1;b>=0;b--){var c=a.snapshotItem(b);c.parentNode.removeChild(c)};
+for(var a=document.evaluate("//a[contains(@href, 'fotball') or contains(@title, 'fotball')]/ancestor::div[contains(@class,'ddCell') or contains(@class,'article-extract') or contains(@class,'gridUnit') or contains(@class,'article_small') or contains(@class,'art_medium')][1]",document,null,XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,null),b=a.snapshotLength-1;b>=0;b--){var c=a.snapshotItem(b);c.parentNode.removeChild(c)};
