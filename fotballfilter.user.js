e921c8b5ceb2ec408757a3e9fbd0cf791cefcfea
unknown <George@.faktab.net>
2012-01-09 09:08:58 +0100

    Første versjon av fotballfilter til Chrome og Fx

---
+++ fotballfilter.user.js
@@ -0,0 +1,12 @@
+﻿// ==UserScript==
+// @name           Fotballfilter
+// @namespace      http://www.okblogg.no
+// @description    Gjør et tappert forsøk på å fjerne fotballnyheter fra nettaviser
+// @match        http://www.dagbladet.no/*
+// @match        http://www.vg.no/*
+// @match        http://www.nrk.no/*
+// @match        http://www.aftenposten.no/*
+// @match        http://www.nettavisen.no/*
+// ==/UserScript==
+
+for(var a=document.evaluate("//a[contains(@href, 'fotball') or contains(@title, 'fotball')]/ancestor::div[contains(@class,'ddCell') or contains(@class,'article-extract') or contains(@class,'gridUnit') or contains(@class,'article_small') or contains(@class,'art_medium')]",document,null,XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,null),b=a.snapshotLength-1;b>=0;b--){var c=a.snapshotItem(b);c.parentNode.removeChild(c)};
